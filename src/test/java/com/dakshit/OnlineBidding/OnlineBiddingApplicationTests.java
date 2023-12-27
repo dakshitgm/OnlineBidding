dac0677c1e501dc117a29886891636e47f1d0485
Dakshit Mahadeshwar <41019458+dakshitgm@users.noreply.github.com>
2023-12-27 13:41:20 +0530

    Initial commit

---
+++ src/test/java/com/dakshit/OnlineBidding/OnlineBiddingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dakshit.OnlineBidding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBiddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
